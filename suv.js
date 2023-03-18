b5ad36831838bd221f94d6e88dc9aea3068fea74
Alexandre Santanna Junior <asantanna144@gmail.com>
2023-03-18 16:21:02 -0300

    rentar.car commit

---
+++ suv.js
@@ -0,0 +1,9 @@
+function calcular(){
+    const dias = document.querySelector('#dias');
+    const km = document.querySelector('#km');
+    const d = Number(dias.value);
+    const k = Number(km.value);
+    const suv = (100 * d) + (0.50 * k);
+    document.querySelector('.resposta').innerHTML = `O valor do aluguel no periodo de ${d} dias e a distância de ${k} Km. </br> Será de: R$ ${suv} reais. `;
+    document.querySelector('.mensagem').innerHTML = `Alugue um carro conosco e experimente a excelência em serviço de locação de veículos. </br> Nossa empresa oferece uma ampla seleção de carros bem-mantidos e atendimento ao </br> cliente excepcional. Não perca mais tempo procurando, escolha a confiabilidade e qualidade </br> que você merece!`;
+    }
